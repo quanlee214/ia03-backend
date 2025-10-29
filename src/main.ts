364c9e6309415a906aa9fd513e6ca3a994d613b8
quanlee214 <22120290@student.hcmus.edu.vn>
2025-10-29 13:26:26 +0700

    fix url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://ia03-frontend-sooty.vercel.app',
+    origin: [
+      'http://localhost:5173',
+      'https://ia03-frontend-sooty.vercel.app' 
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
